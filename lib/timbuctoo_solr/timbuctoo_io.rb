0da3d4b8075f151d37f4fcc37ca186e13f61adc8
Jauco Noordzij <code@jauco.nl>
2017-02-01 14:22:09 +0100

    extract webapp from generic indexer
    It is now an indexer for all the indexes, not just the webapp

--- lib/timbuctoo_solr/timbuctoo_io.rb
+++ lib/timbuctoo_solr/timbuctoo_io.rb
@@ -1,5 +1,4 @@
 require 'net/http'
-require 'httplog'
 require 'json'
 
 def make_http(uri)
---
+++ webserver/web.rb
@@ -0,0 +1,65 @@
+require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/cross_origin'
+require 'httplog'
+
+require_relative '../generic-indexer/generic_indexer'
+require_relative '../federated-indexer/federated_indexer'
+require_relative '../dcar/dutch_caribbean_indexer'
+require_relative '../womenwriters/women_writers_indexer'
+
+options "/trigger" do
+  cross_origin
+  response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+  status 204
+  ""
+end
+post '/trigger' do
+  cross_origin
+  begin
+    payload = JSON.parse(request.body.read)
+    if payload.key?("datasetName")
+      if payload["datasetName"] == "DutchCaribbean"
+        DutchCaribbeanIndexer.new(
+          :timbuctoo_url => ENV['TIMBUCTOO_SCRAPE_URL'],
+          :solr_url => ENV['SOLR_URL']
+        ).run
+        status 200
+        return
+      elsif payload["datasetName"] == "WomenWriters"
+        DutchCaribbeanIndexer.new(
+          :timbuctoo_url => ENV['TIMBUCTOO_SCRAPE_URL'],
+          :solr_url => ENV['SOLR_URL']
+        ).run
+        status 200
+        return
+      else
+        GenericIndexer.new(
+            :vre_id => payload["datasetName"],
+            :timbuctoo_url => ENV['TIMBUCTOO_SCRAPE_URL'],
+            :solr_url => ENV['SOLR_URL']
+        ).run
+        status 200
+        return
+      end
+    end
+    status 400
+  rescue Exception => e
+    status 500
+    raise e
+  end
+end
+post '/trigger-multi-collection-search' do
+  begin
+    payload = JSON.parse(request.body.read)
+    FederatedIndexer.new(
+        :timbuctoo_url => ENV['TIMBUCTOO_SCRAPE_URL'],
+        :solr_url => ENV['SOLR_URL']
+    ).run
+    status 200
+  rescue Exception => e
+    status 500
+    raise e
+  end
+end
